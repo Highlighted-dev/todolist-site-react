9c211814d6d33a6ef0347feb4693294e5d1ef685
n3wbi-dev <bartosz.korz@gmail.com>
2021-05-15 21:22:44 +0200

    added addTask function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 import { useState } from 'react'
 
 function App() {
@@ -23,10 +24,18 @@ function App() {
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
   };
+  const addTask = (task) => {
+    var id = tasks.length + 1;
+    const newTask = { id, ...task  }
+    setTasks([...tasks, newTask])
+    
+  }
   return (
     <div className="container">
+      
       <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/> : 'There are no tasks' }
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'There are no tasks'}
+      <AddTask onAdd={addTask}/>
     </div>
   );
 }
