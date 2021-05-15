af10e45c2e8e1da005403c538ecaaa614a3ecd88
n3wbi-dev <bartosz.korz@gmail.com>
2021-05-15 19:08:20 +0200

    added tasks and header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import { useState } from 'react'
+
 function App() {
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: 'Nakaram kota',
+            date: '15 maja 20:00',
+        },
+        {
+            id: 2,
+            text: 'Zapłać za prąd',
+            date: '30 maja 20:00',
+        },
+        {
+            id: 3,
+            text: "Naucz się na matematykę",
+            date: '16 maja 23:00',
+        }
+    ]);
   return (
-    <div className="App">
-      <h1 align="center">Todolist APP</h1>
+    <div className="container">
+      <Header />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
