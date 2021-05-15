4011d88bf1647cd29af0dce47979f32b1d4bc6dd
n3wbi-dev <bartosz.korz@gmail.com>
2021-05-15 21:22:13 +0200

    added AddTask.js

---
+++ src/components/AddTask.js
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+
+const AddTask = ({ onAdd }) => {
+    const [text, setText] = useState('');
+    const [date, setDate] = useState('');
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        if (!text) {
+            alert('Please add a task');
+            return
+        }
+        else if (!date) {
+            alert('Please add a task');
+            return
+        }
+
+        onAdd({ text, date });
+        setText('');
+        setDate('');
+            
+    }
+
+    return (
+        <form className='add-form' onSubmit={onSubmit }>
+            <div className='form-control'>
+                <label>Task</label>
+                <input type='text' placeholder='Add Task' value={text}
+                    onChange={(e) => setText(e.target.value)}/>
+            </div>
+            <div className='form-control'>
+                <label>Date</label>
+                <input type='text' placeholder='Add date' value={date}
+                    onChange={(e) => setDate(e.target.value)}/>
+            </div>
+            <input type="submit" value='Save Task' className='btn btn-block' />
+        </form>
+    )
+}
+
+export default AddTask
