1da06d639226ac957a15f6527a5629658327661d
n3wbi-dev <bartosz.korz@gmail.com>
2021-05-15 21:41:24 +0200

    Added onClick to button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
-const Header = () => {
+const Header = ({ onAdd }) => {
     return (
         <header className='header'>
             <h1 align="center">Todolist App</h1>
-            <button className="btn">Add</button>
+            <button className="btn" onClick={onAdd}>Add Task</button>
         </header>
     )
 }
