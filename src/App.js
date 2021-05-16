c5825960dc8ef689bb7434abf8284e8e41c317d7
n3wbi-dev <bartosz.korz@gmail.com>
2021-05-16 14:37:15 +0200

    Tasks are now stored on json-server, added bunch of comments

--- src/App.js
+++ src/App.js
@@ -1,34 +1,48 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
-    const [showAddTask, setShowAddTask] = useState(true)
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            text: 'Nakaram kota',
-            date: '15 maja 20:00',
-        },
-        {
-            id: 2,
-            text: 'Zapłać za prąd',
-            date: '30 maja 20:00',
-        },
-        {
-            id: 3,
-            text: "Naucz się na matematykę",
-            date: '16 maja 23:00',
-        }
-    ]);
-  const deleteTask = (id) => {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([]);
+
+  //Get tasks from server
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer) 
+    }
+    getTasks()
+  }, []);
+
+  //Fetch data from server to json file
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks');
+    const data = await res.json();
+    
+    return data
+  }
+
+  //Delete task
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, { method: 'DELETE' })
     setTasks(tasks.filter((task) => task.id !== id))
   };
-  const addTask = (task) => {
-    var id = tasks.length + 1;
-    const newTask = { id, ...task  }
-    setTasks([...tasks, newTask])  
+
+  //Add task
+  const addTask = async(task) => {
+
+    const res = await fetch(`http://localhost:5000/tasks/`, {
+      method: 'POST',
+      headers: {
+      'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+    const data = await res.json()
+    setTasks([...tasks,data]) 
   }
 
   return (
